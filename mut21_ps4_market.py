c6f3c7db5d0dc1a2cf648a896f6468ae1ff8ce6a
Ruby Swammy <mjivan7@gmail.com>
2020-10-28 11:00:27 -0700

    Prompt added for ovr or string

--- mut21_ps4_market.py
+++ mut21_ps4_market.py
@@ -15,14 +15,15 @@ ps4_86 = 'https://www.muthead.com/21/players/?overall__gte=86&overall__lte=86&pr
 ps4_87 = 'https://www.muthead.com/21/players/?overall__gte=87&overall__lte=87&price_platform=2&sort_by=summary_price'
 ps4_88 = 'https://www.muthead.com/21/players/?overall__gte=88&overall__lte=88&price_platform=2&sort_by=summary_price'
 ps4_89 = 'https://www.muthead.com/21/players/?overall__gte=89&overall__lte=89&price_platform=2&sort_by=summary_price'
-
 ps4_90 = 'https://www.muthead.com/21/players/?overall__gte=90&overall__lte=91&price_platform=2&sort_by=summary_price'
 ps4_91 = 'https://www.muthead.com/21/players/?overall__gte=91&overall__lte=91&price_platform=2&sort_by=summary_price'
+ps4_92 = 'https://www.muthead.com/21/players/?overall__gte=92&overall__lte=92&price_platform=2&sort_by=summary_price'
+ps4_93 = 'https://www.muthead.com/21/players/?overall__gte=93&overall__lte=93&price_platform=2&sort_by=summary_price'
 
-most_feared_ = 'https://www.muthead.com/21/players/?overall__gte=92&program=176&sort_by=summary_price'
+most_feared_ = 'https://www.muthead.com/21/players/?overall__gte=90&overall__lte=90&price_platform=2&program=176&sort_by=name'
 
 
-print("Search in progress...")
+# print("Search in progress...")
 
 # PS4 Elite Cards
 
@@ -36,16 +37,37 @@ def getRequest(cardOvr, ovr):
 
     print(str(ovr) + "-> " + cheapest_player_name.string + " \t" + cheapest_player_price.string + " " + cheapest_player_info.string)
 
-getRequest(most_feared_, 92)
-getRequest(ps4_80, 80)
-getRequest(ps4_81, 81)
-# getRequest(ps4_82, 82)
-getRequest(ps4_83, 83)
-getRequest(ps4_84, 84)
-getRequest(ps4_85, 85)
-getRequest(ps4_86, 86)
-# getRequest(ps4_87, 87)
-getRequest(ps4_88, 88)
-getRequest(ps4_89, 89)
-getRequest(ps4_90, 90)
-getRequest(ps4_91, 91)
+ovr_request = input("Enter Ovr or string: ")
+print("Finding cheapest " + ovr_request + "\n")
+if(ovr_request == 'most feared'):
+    getRequest(most_feared_, 90)
+elif(ovr_request == '80'):
+    getRequest(ps4_80, 80)  
+elif(ovr_request == '81'):
+    getRequest(ps4_81, 81)
+elif(ovr_request == '82'):
+    getRequest(ps4_82, 82)
+elif(ovr_request == '83'):
+    getRequest(ps4_83, 83)
+elif(ovr_request == '84'):
+    getRequest(ps4_84, 84)
+elif(ovr_request == '85'):
+    getRequest(ps4_85, 85)
+elif(ovr_request == '86'):
+    getRequest(ps4_86, 86)
+elif(ovr_request == '87'):
+    getRequest(ps4_87, 87)
+elif(ovr_request == '88'):
+    getRequest(ps4_88, 88)
+elif(ovr_request == '89'):
+    getRequest(ps4_89, 89)
+elif(ovr_request == '90'):
+    getRequest(ps4_90, 90)
+elif(ovr_request == '91'):
+    getRequest(ps4_91, 91)
+elif(ovr_request == '92'):
+    getRequest(ps4_92, 92)
+elif(ovr_request == '93'):
+    getRequest(ps4_93, 93)
+else:
+    print("incorrect overall must be in range from 80-93")
